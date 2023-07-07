780d87e9d30a84b0872758a205fb1bad5928e399
primenumber <prime@kmc.gr.jp>
2023-07-08 01:11:50 +0900

    WIP: implement new greedy algorithm

--- src/greedy.rs
+++ src/greedy.rs
@@ -1,18 +1,47 @@
 use crate::common::*;
 use anyhow::Result;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum SolveGreedyError {
+    #[error("Lack of candidates")]
+    LackCandidatesError,
+}
+
+pub fn solve_greedy_impl(
+    musician: u32,
+    attendees: &[Attendee],
+    candidates: &mut Vec<Point>,
+) -> Result<Point> {
+    let (idx, score) = candidates
+        .iter()
+        .enumerate()
+        .max_by_key(|(_, &place)| gain(musician, attendees, place))
+        .ok_or(SolveGreedyError::LackCandidatesError)?;
+    Ok(candidates.swap_remove(idx))
+}
 
 pub fn solve_greedy(prob: &Problem) -> Result<Solution> {
     let musician_left = prob.stage_bottom_left[0] + 10.;
     let musician_bottom = prob.stage_bottom_left[1] + 10.;
     let musicians_in_row = ((prob.stage_width - 10.) / 10.).floor() as usize;
+    let musicians_in_col = ((prob.stage_height - 10.) / 10.).floor() as usize;
+    let mut placement_candidates = Vec::new();
+    for row in 0..musicians_in_col {
+        for col in 0..musicians_in_col {
+            placement_candidates.push(Point {
+                x: musician_left + col as f64 * 10.,
+                y: musician_bottom + row as f64 * 10.,
+            });
+        }
+    }
     let mut placements = Vec::new();
-    for (i, _) in prob.musicians.iter().enumerate() {
-        let row = i / musicians_in_row;
-        let col = i % musicians_in_row;
-        placements.push(Point {
-            x: musician_left + col as f64 * 10.,
-            y: musician_bottom + row as f64 * 10.,
-        });
+    for &musician in &prob.musicians {
+        placements.push(solve_greedy_impl(
+            musician,
+            &prob.attendees,
+            &mut placement_candidates,
+        )?);
     }
     Ok(Solution { placements })
 }
