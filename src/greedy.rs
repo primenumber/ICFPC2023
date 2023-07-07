b763181c15a71481ff9996ff07518a328f8c3f7c
primenumber <prime@kmc.gr.jp>
2023-07-08 05:26:08 +0900

    Adjustable distance

--- src/greedy.rs
+++ src/greedy.rs
@@ -37,16 +37,20 @@ fn solve_greedy_impl(
 pub fn solve_greedy(prob: &Problem) -> Result<Solution> {
     let musician_left = prob.stage_bottom_left[0] + 10.;
     let musician_bottom = prob.stage_bottom_left[1] + 10.;
-    let musicians_in_row = ((prob.stage_width - 10.) / 10.).floor() as usize;
-    let musicians_in_col = ((prob.stage_height - 10.) / 10.).floor() as usize;
+    let min_distance = 10.;
+    let musicians_in_row = ((prob.stage_width - 10.) / min_distance).floor() as usize;
+    let musicians_in_col = ((prob.stage_height - 10.) / min_distance).floor() as usize;
     let mut placement_candidates = Vec::new();
     for row in 0..musicians_in_col {
         for col in 0..musicians_in_row {
             placement_candidates.push(Point {
-                x: musician_left + col as f64 * 10.,
-                y: musician_bottom + row as f64 * 10.,
+                x: musician_left + col as f64 * min_distance,
+                y: musician_bottom + row as f64 * min_distance,
             });
         }
+    }
+    if placement_candidates.len() < prob.musicians.len() {
+        return Err(SolveGreedyError::LackCandidatesError.into());
     }
     let mut placements = Vec::new();
     for &musician in &prob.musicians {
