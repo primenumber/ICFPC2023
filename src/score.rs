375978125da3c358da6feafe62b10b869cc067b5
primenumber <prime@kmc.gr.jp>
2023-07-08 21:35:27 +0900

    Check musicians are in the stage

--- src/score.rs
+++ src/score.rs
@@ -46,7 +46,7 @@ fn happiness(attendee: &Attendee, musicians: &[u32], sol: &Solution) -> i64 {
 pub fn score(prob: &Problem, sol: &Solution) -> Result<i64> {
     let n: usize = prob.attendees.len();
 
-    if !is_valid_answer(sol) {
+    if !is_valid_answer(prob, sol) {
         println!("invalid solution");
         return Ok(0);
     }
@@ -63,9 +63,23 @@ pub fn score(prob: &Problem, sol: &Solution) -> Result<i64> {
     Ok(score)
 }
 
-fn is_valid_answer(sol: &Solution) -> bool {
+fn is_valid_answer(prob: &Problem, sol: &Solution) -> bool {
     let mut is_valid = true;
+    let stage_left = prob.stage_bottom_left[0];
+    let stage_bottom = prob.stage_bottom_left[1];
+    let stage_right = stage_left + prob.stage_width;
+    let stage_top = stage_bottom + prob.stage_height;
     for (musician_idx, musician_point) in sol.placements.iter().enumerate() {
+        if musician_point.x < stage_left + 10.
+            || musician_point.x > stage_right - 10.
+            || musician_point.y < stage_bottom + 10.
+            || musician_point.y > stage_top - 10.
+        {
+            is_valid = false;
+            println!(
+                "{musician_idx} is not inside the stage or too close to the edge of the stage"
+            );
+        }
         for (check_musician_idx, check_musician_point) in sol.placements.iter().enumerate() {
             if musician_idx == check_musician_idx {
                 continue;
