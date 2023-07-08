f3c9a20a95591a1a1a09b6653e5e781134c1298c
primenumber <prime@kmc.gr.jp>
2023-07-08 10:50:47 +0900

    Refine score

--- src/score.rs
+++ src/score.rs
@@ -1,74 +1,66 @@
+use crate::common::*;
 use anyhow::Result;
 use indicatif::ProgressBar;
 use serde::{Deserialize, Serialize};
 
 use crate::common::{Point, Problem, Solution};
 
-pub fn score(prob: &Problem, sol: &Solution) -> Result<()> {
+fn impact(attendee: &Attendee, kinds: &[u32], placements: &[Point], musician_idx: usize) -> i64 {
+    let attendee_point = Point {
+        x: attendee.x,
+        y: attendee.y,
+    };
+    let place = placements[musician_idx];
+    let kind = kinds[musician_idx];
+    for (check_musician_idx, check_place) in placements.iter().enumerate() {
+        if musician_idx == check_musician_idx {
+            continue;
+        }
+        let musician_attendee_line = Line {
+            p1: Point {
+                x: attendee.x,
+                y: attendee.y,
+            },
+            p2: place,
+        };
+        let check_musician_area = Circle {
+            c: *check_place,
+            r: 5.0,
+        };
+        if is_cross_line_circle(&musician_attendee_line, &check_musician_area) {
+            return 0;
+        }
+    }
+    let dsq = (attendee_point - place).norm();
+    (1e6 * attendee.tastes[kind as usize] / dsq).ceil() as i64
+}
+
+fn happiness(attendee: &Attendee, musicians: &[u32], sol: &Solution) -> i64 {
+    let mut score = 0;
+    for (musician_idx, _) in musicians.iter().enumerate() {
+        score += impact(attendee, &musicians, &sol.placements, musician_idx);
+    }
+    score
+}
+
+pub fn score(prob: &Problem, sol: &Solution) -> Result<i64> {
     let n: usize = prob.attendees.len();
 
     if !is_valid_answer(sol) {
         println!("invalid solution");
-        return Ok(());
+        return Ok(0);
     }
 
     let mut score: i64 = 0;
 
     let pb = ProgressBar::new(n as u64);
-    'attendee: for attendee in prob.attendees.iter() {
-        let attendee_point = Point {
-            x: attendee.x,
-            y: attendee.y,
-        };
-        let musicians: Vec<(usize, (&Point, &u32))> = sol
-            .placements
-            .iter()
-            .zip(prob.musicians.iter())
-            .enumerate()
-            .collect();
-        for (_musician_idx, musician) in musicians.iter() {
-            let musician_area = Circle {
-                c: *musician.0,
-                r: 5.0,
-            };
-            if is_point_in_circle(&musician_area, &attendee_point) {
-                let dsq = (attendee_point - *musician.0).norm();
-                score += (1e6 * attendee.tastes[*musician.1 as usize] / dsq).ceil() as i64;
-                pb.inc(1);
-                continue 'attendee;
-            }
-        }
-        for (musician_idx, musician) in &musicians {
-            let mut valid_impact = true;
-            for (check_musician_idx, check_musician) in &musicians {
-                if musician_idx == check_musician_idx {
-                    continue;
-                }
-                let musician_attendee_line = Line {
-                    p1: Point {
-                        x: attendee.x,
-                        y: attendee.y,
-                    },
-                    p2: *musician.0,
-                };
-                let check_musician_area = Circle {
-                    c: *check_musician.0,
-                    r: 5.0,
-                };
-                if is_cross_line_circle(&musician_attendee_line, &check_musician_area) {
-                    valid_impact = false;
-                }
-            }
-            if valid_impact {
-                let dsq = (attendee_point - *musician.0).norm();
-                score += (1e6 * attendee.tastes[*musician.1 as usize] / dsq).ceil() as i64;
-            }
-        }
+    for attendee in prob.attendees.iter() {
+        score += happiness(&attendee, &prob.musicians, sol);
         pb.inc(1);
     }
     pb.finish_with_message("finish calculation");
     println!("score {score}");
-    Ok(())
+    Ok(score)
 }
 
 fn is_valid_answer(sol: &Solution) -> bool {
@@ -97,12 +89,6 @@ struct Line {
 struct Circle {
     c: Point,
     r: f64,
-}
-
-fn is_point_in_circle(circle: &Circle, point: &Point) -> bool {
-    let center = circle.c;
-    let dsq: f64 = (center - *point).norm();
-    dsq < circle.r * circle.r
 }
 
 fn is_cross_line_circle(line: &Line, circle: &Circle) -> bool {
