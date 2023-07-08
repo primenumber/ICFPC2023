298fb934579458edcb18f9353c9da013716180bc
primenumber <prime@kmc.gr.jp>
2023-07-08 19:52:05 +0900

    Implement new greedy

--- src/greedy.rs
+++ src/greedy.rs
@@ -1,6 +1,8 @@
 use crate::common::*;
 use anyhow::Result;
-use std::cmp::Reverse;
+use ordered_float::NotNan;
+use std::cmp::{Ordering, Reverse};
+use std::collections::{BinaryHeap, HashMap};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -22,7 +24,90 @@ fn gain(musician: u32, attendees: &[Attendee], place: Point) -> i64 {
     sum
 }
 
-pub fn solve_greedy(prob: &Problem) -> Result<Solution> {
+fn argcmp(p0: Point, p1: Point) -> Ordering {
+    ((p0.y, p0.x) < (0., 0.))
+        .cmp(&((p1.y, p1.x) < (0., 0.)))
+        .then_with(|| (p1.x * p0.y).partial_cmp(&(p0.x * p1.y)).unwrap())
+}
+
+fn visible_attendees(attendees: &[Attendee], candidate: Point, placed: &[Point]) -> Vec<usize> {
+    let mut attendee_angles = Vec::new();
+    for (idx, attendee) in attendees.iter().enumerate() {
+        let d = Point {
+            x: attendee.x,
+            y: attendee.y,
+        } - candidate;
+        attendee_angles.push((d.y.atan2(d.x), d.norm(), idx));
+    }
+    attendee_angles.sort_by(|lhs, rhs| lhs.partial_cmp(rhs).unwrap());
+    let mut blocked_ranges = Vec::new();
+    let block_radius = 5.;
+    let tau = std::f64::consts::TAU;
+    for &place in placed.iter() {
+        let diff = place - candidate;
+        let distance = diff.length();
+        let angle = diff.y.atan2(diff.x);
+        let delta = (block_radius / distance).asin();
+        let from = angle - delta;
+        let to = angle + delta;
+        blocked_ranges.push((from, to, distance * delta.cos()));
+        if to >= tau {
+            blocked_ranges.push((from - tau, to - tau, distance * delta.cos()));
+        }
+        if from < 0. {
+            blocked_ranges.push((from + tau, to + tau, distance * delta.cos()));
+        }
+    }
+    let mut itr = attendee_angles.iter().peekable();
+    blocked_ranges.sort_by(|lhs, rhs| lhs.partial_cmp(rhs).unwrap());
+    let mut heap = BinaryHeap::<(Reverse<NotNan<f64>>, NotNan<f64>)>::new();
+    let mut res = Vec::new();
+    for (from, to, di) in blocked_ranges {
+        loop {
+            let Some(&(angle, dj, j)) = itr.peek() else { break };
+            if angle > &from {
+                break;
+            }
+            let _ = itr.next();
+            let mut to_be_pushed = true;
+            while let Some((dk, to_k)) = heap.peek() {
+                if to_k.into_inner() < *angle {
+                    heap.pop();
+                    continue;
+                }
+                if dk.0.into_inner() <= *dj {
+                    to_be_pushed = false;
+                }
+                break;
+            }
+            if to_be_pushed {
+                res.push(*j);
+            }
+        }
+        heap.push((Reverse(NotNan::new(di).unwrap()), NotNan::new(to).unwrap()));
+    }
+    loop {
+        let Some(&(angle, dj, j)) = itr.peek() else { break };
+        let _ = itr.next();
+        let mut to_be_pushed = true;
+        while let Some((dk, to_k)) = heap.peek() {
+            if to_k.into_inner() < *angle {
+                heap.pop();
+                continue;
+            }
+            if dk.0.into_inner() <= *dj {
+                to_be_pushed = false;
+            }
+            break;
+        }
+        if to_be_pushed {
+            res.push(*j);
+        }
+    }
+    res
+}
+
+fn generate_candidates(prob: &Problem) -> Result<Vec<Point>> {
     let musician_left = prob.stage_bottom_left[0] + 10.;
     let musician_bottom = prob.stage_bottom_left[1] + 10.;
     let min_distance = 10.;
@@ -40,28 +125,40 @@ pub fn solve_greedy(prob: &Problem) -> Result<Solution> {
     if placement_candidates.len() < prob.musicians.len() {
         return Err(SolveGreedyError::LackCandidatesError.into());
     }
-    let mut gains = Vec::new();
-    for (i, &musician) in prob.musicians.iter().enumerate() {
-        for (j, &place) in placement_candidates.iter().enumerate() {
-            gains.push((gain(musician, &prob.attendees, place), i, j));
-        }
-    }
-    gains.sort_unstable_by_key(|e| Reverse(e.0));
-    let mut musician_used = vec![false; prob.musicians.len()];
-    let mut place_used = vec![false; placement_candidates.len()];
+    Ok(placement_candidates)
+}
+
+pub fn solve_greedy(prob: &Problem) -> Result<Solution> {
+    let mut placement_candidates = generate_candidates(prob)?;
+    let mut placed = Vec::new();
+    let mut musicians: HashMap<_, _> = prob.musicians.iter().enumerate().collect();
     let mut pairs = Vec::new();
-    for (_g, i, j) in gains {
-        if musician_used[i] || place_used[j] {
-            continue;
+    while !musicians.is_empty() {
+        let mut max_gain = i64::MIN;
+        let mut max_gain_pidx = 0;
+        let mut max_gain_midx = 0;
+        for (pidx, &place) in placement_candidates.iter().enumerate() {
+            let atd_indices = visible_attendees(&prob.attendees, place, &placed);
+            for (midx, &&kind) in &musicians {
+                let mut impact_sum = 0;
+                for &aidx in &atd_indices {
+                    let atd = &prob.attendees[aidx];
+                    let dsq = (Point { x: atd.x, y: atd.y } - place).norm();
+                    impact_sum += (1e6 * atd.tastes[kind as usize] / dsq).ceil() as i64;
+                }
+                if impact_sum > max_gain {
+                    max_gain = impact_sum;
+                    max_gain_pidx = pidx;
+                    max_gain_midx = *midx;
+                }
+            }
         }
-        pairs.push((i, j));
-        musician_used[i] = true;
-        place_used[j] = true;
+        let place = placement_candidates.swap_remove(max_gain_pidx);
+        placed.push(place);
+        musicians.remove(&max_gain_midx);
+        pairs.push((max_gain_midx, place));
     }
     pairs.sort_unstable_by_key(|e| e.0);
-    let placements = pairs
-        .into_iter()
-        .map(|(_, j)| placement_candidates[j])
-        .collect();
+    let placements = pairs.into_iter().map(|(_, place)| place).collect();
     Ok(Solution { placements })
 }
